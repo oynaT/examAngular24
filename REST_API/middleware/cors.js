5349f7b4b029cd4c93592202e9d2bcea0b044e91
oynaT <98087915+oynaT@users.noreply.github.com>
2024-03-29 20:00:48 +0200

    Delete REST_API directory

--- REST_API/middleware/cors.js
+++
@@ -1,6 +0,0 @@
-module.exports = () => (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, HEAD,PUT, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization');
-    next();
-}
